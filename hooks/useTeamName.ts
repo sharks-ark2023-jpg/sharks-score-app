06ca9b357418d2a8b84601d4a33e3064e52dac50
JSK <jskark@JSK-no-MacBook-Pro.local>
2026-01-11 23:43:21 +0900

    Remove remaining legacy directories (hooks, types, services) to fix build

--- hooks/useTeamName.ts
+++
@@ -1,9 +0,0 @@
-import useLocalStorage from './useLocalStorage';
-
-const DEFAULT_TEAM_NAME = "マイチームFC";
-
-export const useTeamName = () => {
-    const [teamName, setTeamName] = useLocalStorage('teamName', DEFAULT_TEAM_NAME);
-
-    return { teamName: teamName || DEFAULT_TEAM_NAME, setTeamName };
-};
